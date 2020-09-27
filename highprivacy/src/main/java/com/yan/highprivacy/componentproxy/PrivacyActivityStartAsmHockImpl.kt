ce6e43b37faa8992d0dd432ab8080d3c6a7ffeea
Bevan <xianwei.yan@quvideo.com>
2020-09-27 17:51:44 +0800

    增加 startActivity拦截 切换 判断

--- highprivacy/src/main/java/com/yan/highprivacy/componentproxy/PrivacyActivityStartAsmHockImpl.kt
+++ highprivacy/src/main/java/com/yan/highprivacy/componentproxy/PrivacyActivityStartAsmHockImpl.kt
@@ -11,26 +11,49 @@ import com.yan.highprivacy.PrivacyMgr
  * startActivity 拦截，插桩在activity onCreate的实现
  */
 class PrivacyActivityStartAsmHockImpl : PrivacyActivityStartAsmHock, PrivacyCallActivityStart {
-    companion object {
-        @JvmStatic
-        val ins by lazy { PrivacyActivityStartAsmHockImpl() }
+  companion object {
+    @JvmStatic
+    val ins by lazy { PrivacyActivityStartAsmHockImpl() }
+  }
+
+  private var callStartActImpl: PrivacyCallActivityStart? = null
+
+  init {
+    if (!PrivacyMgr.privacyMgr.isStartActWithInstrument) {
+      callStartActImpl = PrivacyCallActivityStartRealImpl(this)
     }
+  }
 
-    private var callStartActImpl = PrivacyCallActivityStartRealImpl(this)
+  override fun callPrivacyActClass(
+    className: String,
+    intent: Intent?
+  ) = className
 
-    override fun callPrivacyActClass(className: String, intent: Intent?) = className
+  override fun callOriginalPrivacyActStart(context: Context) {
+  }
 
-    override fun callOriginalPrivacyActStart(context: Context) {
+  override fun interceptOriginalActProcess(
+    context: Context,
+    actClazz: Class<*>
+  ): Boolean {
+    val callStart = callStartActImpl ?: return false
+
+    if (PrivacyMgr.privacy.isAuth()) return false
+    val className =
+      callStart.callPrivacyActClass(
+          actClazz.name,
+          Intent(context, actClazz).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      )
+    val isIntercept = actClazz.name != className
+    if (isIntercept) {
+      val privacyClass = PrivacyMgr.privacy.highPrivacyActivityClass
+      requireNotNull(privacyClass) { "highPrivacyActivityClass must be set in PrivacyMgr" }
+      context.startActivity(
+          Intent().setClass(context, privacyClass)
+              .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      )
     }
-
-    override fun interceptOriginalActProcess(context: Context, actClazz: Class<*>): Boolean {
-        if (PrivacyMgr.privacy.isAuth()) return false
-        val className =
-            callStartActImpl.callPrivacyActClass(
-                actClazz.name,
-                Intent(context, actClazz).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            )
-        return actClazz.name != className
-    }
+    return isIntercept
+  }
 
 }
